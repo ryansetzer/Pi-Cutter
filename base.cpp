4b560c9863fd88c3a5cd8ed37c10b0cf14f1fd64
Ryan Setzer <rsetzah@wozniak.local>
2022-12-21 11:41:21 -0500

    basic message class

--- base.cpp
+++ base.cpp
@@ -1,7 +1,31 @@
 #include<iostream>
 using namespace std;
 
+class message {
+    public:
+        uint8_t id;
+        uint8_t type;
+        uint8_t flags;
+        uint8_t sendType;
+        message() {
+            id = -1;
+            type = -1;
+            flags = -1;
+            sendType = -1;
+        }
+        void printAllFields() {
+            printf ("id:\t%d\n", id);
+            printf ("type:\t%d\n", type);
+            printf ("flags:\t%d\n", flags);
+            printf ("sendType:\t%d\n", sendType);
+        }
+};
+
 int main()
 {
+    message msg;
+    msg.id = 1;
+    msg.printAllFields();
+    printf ("Hello\n");
     return 0;
 }
